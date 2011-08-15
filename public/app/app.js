2bc640dc70fed37781ec7a5a25dfb7cdcac1f546
Kurt Harriger <kurtharriger@gmail.com>
2011-08-14 19:03:28 -0600

    fix localstorage not cleared

--- public/app/app.js
+++ public/app/app.js
@@ -1,8 +1,13 @@
 App = (function() {
 	return new Ext.Application({
     	name: "App",
+
+          clearLocalStorage: function() {
+            localStorage.clear();
+          },
 		
 	    launch: function() {
+                App.stores.events.on('clear', this.clearLocalStorage);
 	        this.views.viewport = new this.views.Viewport();
 	    }
 	});
