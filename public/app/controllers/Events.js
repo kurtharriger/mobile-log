c6b56aeba25dd64fd79957745b0cca06398f118f
Kurt Harriger <kurtharriger@gmail.com>
2011-08-14 18:34:00 -0600

    implement clear button

--- public/app/controllers/Events.js
+++ public/app/controllers/Events.js
@@ -1,10 +1,14 @@
 App.controllers.Events = (function() {
-	return Ext.regController('Events', {
+
+  return  Ext.regController('Events', {
 		store: App.stores.events,
 	
-	    newEvent: function() {
-			this.store.create({ datetime: new Date(), lat: 0, lon:0 } );
-	    }
+    	       newEvent: function() {
+                 this.store.create({ datetime: new Date(), lat: 0, lon:0 } );
+               },
+               clearEvents: function() {
+                 this.store.removeAll();
+               }
 
 	});
 })();
--- public/app/views/Viewport.js
+++ public/app/views/Viewport.js
@@ -1,14 +1,19 @@
 App.views.Viewport = (function() {
-	var addButton, titlebar, list, onAddAction;
-
-    onAddAction = function() {
+    var onAddAction = function() {
       Ext.dispatch({
           controller: 'Events',
           action: 'newEvent'
       });
     };
 
-    addButton = {
+    var onClearAction = function() {
+      Ext.dispatch({
+        controller: 'Events',
+        action: 'clearEvents'
+      });
+    };
+
+    var addButton = {
         itemId: 'addButton',
         iconCls: 'add',
         iconMask: true,
@@ -16,29 +21,47 @@ App.views.Viewport = (function() {
         handler: onAddAction,
     };
 
-    titlebar = {
+    var clearButton = { 
+      text: 'Clear',
+      iconMask: true,
+      iconCls: 'trash',
+      ui: 'round',
+      handler: onClearAction
+    };
+
+    var titlebar = {
         dock: 'top',
         xtype: 'toolbar',
         title: 'Events',
         items: [ { xtype: 'spacer' }, addButton ]
     };
 
-    footer = {
-        dock: 'bottom',
-		html: 'Copyright (c) 2011, Kurt Harriger. All Rights Reserved.'
+    var list = {
+       xtype: 'list',
+       itemTpl: '{datetime}',
+       store: App.stores.events
     };
 
-
-    list = {
-        xtype: 'list',
-        itemTpl: '{datetime}',
-        store: App.stores.events
+    var footbar = {
+      dock: 'bottom',
+      xtype: 'toolbar',
+      ui: 'light',
+      //layout: { pack: 'center' },
+      items: [{xtype: 'spacer'},  clearButton] 
     };
+
+    var footer = {
+      dock: 'bottom',
+      html: 'Copyright (c) 2011, Kurt Harriger. All Rights Reserved.'
+    };
+
+    
+
 
 	return Ext.extend(Ext.Panel, {
 	    fullscreen: true,
 	    layout: 'fit',
-	    dockedItems: [titlebar, footer],
+	    dockedItems: [titlebar, footer, footbar],
 	    items: [list],
 		onAddAction: onAddAction
 	});
